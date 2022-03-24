ac7639f85dc69380e925508b0c6952c4657f7066
Cheryl See <cheryl.see@revature.net>
2022-03-24 15:08:35 -0500

    Fixed spring to serve index page

--- src/main/java/com/ans/funstuff/beans/controllers/MainController.java
+++ src/main/java/com/ans/funstuff/beans/controllers/MainController.java
@@ -3,23 +3,33 @@ package com.ans.funstuff.beans.controllers;
 import com.ans.funstuff.beans.services.BookmarkService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class MainController {
     private BookmarkService bookmarkService;
     private ObjectMapper mapper;
 
     @GetMapping("/")
     public String getRoot() {
-        return "This is root";
+        return "redirect:index.html";
     }
 
+    @ResponseBody
     @PostMapping("/add")
     public String addUser() {
         return "New bookmark added?";
+    }
+
+    @ResponseBody
+    @GetMapping("/id")
+    public String getId() {
+        return "This is some id";
     }
 
     @Autowired
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,5 @@
-const url = "http://localhost:8080";
+//const url = "http://localhost:8080";
+const url = "https://thursday-coffee.azurewebsites.net/";
 
 function doAddBookmark(info) {
 	let responseData;
@@ -16,9 +17,9 @@ function doAddBookmark(info) {
 document.getElementById("submit-new-button").addEventListener("click", () => {
 	console.log("button clicked");
 	const info = {
-		name: document.getElementById("name").value,
-		url: document.getElementById("url").value,
-		description: document.getElementById("description").value,
+		name: document.getElementById("name-field").value,
+		url: document.getElementById("url-field").value,
+		description: document.getElementById("description-field").value,
 	};
 	console.log(info);
 	doAddBookmark(info);
