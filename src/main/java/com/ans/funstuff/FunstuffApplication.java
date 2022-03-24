c73e2c7e6ae205544448947ad2fd9623985d8754
Cheryl See <cheryl.see@revature.net>
2022-03-24 09:26:59 -0500

    First steps

---
+++ src/main/java/com/ans/funstuff/FunstuffApplication.java
@@ -0,0 +1,13 @@
+package com.ans.funstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunstuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunstuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ans/funstuff/FunstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ans.funstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunstuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
